f9aec7c4c535904928578b04a337ea64339c4965
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-06-06 12:48:28 +0530

    feat: realtime polling messages

---
+++ polling/exercise/frontend/polling-chat.js
@@ -0,0 +1,75 @@
+const chat = document.getElementById("chat");
+const msgs = document.getElementById("msgs");
+
+let allChat = [];
+
+const INTERVAL = 3000;
+
+chat.addEventListener("submit", function (e) {
+  e.preventDefault();
+  postNewMsg(chat.elements.user.value, chat.elements.text.value);
+  chat.elements.text.value = "";
+});
+
+async function postNewMsg(user, text) {
+  const data = {
+    user,
+    text,
+  };
+
+  // request options
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  // send POST request
+  const res = await fetch("/poll", options);
+  const json = await res.json();
+
+  console.log("success", json);
+}
+
+async function getNewMsgs() {
+  let json;
+  try {
+    const res = await fetch("/poll");
+    json = await res.json();
+
+    if (res.status >= 400) {
+      throw new Error("Request did not succed", res.status);
+    }
+    allChat = json.msg;
+    render();
+    failedTries = 0;
+  } catch (e) {
+    // back off
+    failedTries++;
+    console.error("polling error", e);
+  }
+}
+
+function render() {
+  const html = allChat.map(({ user, text }) => template(user, text));
+  msgs.innerHTML = html.join("\n");
+}
+
+const template = (user, msg) =>
+  `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
+
+let timeToMakeNextRequest = 0;
+const BACKOFF = 5000;
+let failedTries = 0;
+
+async function rafTimer(time) {
+  if (timeToMakeNextRequest <= time) {
+    getNewMsgs();
+    timeToMakeNextRequest = time + INTERVAL + failedTries * BACKOFF;
+  }
+  requestAnimationFrame(rafTimer);
+}
+
+requestAnimationFrame(rafTimer);
